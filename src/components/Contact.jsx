99ae7eacb66d1168aebfea3fd48b1f4d83fc0e1d
PHẠM NGUYỄN NHẬT TÂN <nguyenthitanh1309@gmail.com>
2026-05-05 09:27:26 +0700

    fix responsive mobile

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ function Contact() {
   return (
     <section className="contact-section" id="contact">
       <div className="contact-wrapper">
-        <h2 className="section-title contact-title">Liên hệ</h2>
+        <h2 className="section-title contact-title">Liên hệ Ngay (test)</h2>
         <p className="contact-desc">
           Bạn có ý tưởng muốn thực hiện, hãy giao cho tôi, tôi sẽ biến nó thành sự thật.
         </p>
